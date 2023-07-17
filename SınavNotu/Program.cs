fd1f5c52ca473d342d6549120e0aa2bdcf8ee28d
Büşra Melek Uzdu <busramelekuzdu@gmail.com>
2023-07-17 12:22:26 +0300

    Proje dosyası ekle.

---
+++ SınavNotu/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SınavNotu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int vize, final;
+            double sonuc;
+            Console.Write("Vize notunu giriniz:");
+            vize= Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Final notunuz:");
+            final= Convert.ToInt32(Console.ReadLine());
+
+            sonuc = final * 0.7 + vize * 0.4;
+
+            if (sonuc < 50)
+                Console.WriteLine("Ortalamanız:"+sonuc+" Sınavdan kaldınız.");
+            else
+                Console.WriteLine("Ortalamanız:" + sonuc + " Sınavdan geçtiniz.");
+
+            Console.ReadLine();
+        }
+    }
+}
